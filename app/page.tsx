434d32990bd7ec434c8fb94bef992fc66bc0a3c6
lipon <liponbdbd@gmail.com>
2025-07-30 17:00:29 +0600

    test ci/cd 4

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function HomePage() {
         >
           Capitalize
         </button>
-        {capitalized && <p className="mt-2">Resultssssss: {capitalized}</p>}
+        {capitalized && <p className="mt-2">Result1: {capitalized}</p>}
       </div>
 
       <div className="mt-6">
